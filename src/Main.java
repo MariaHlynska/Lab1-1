03058808c88d4e18a53552e398639167548494a8
Maria Hlynska <hlynskamasha@gmail.com>
2023-10-31 21:38:40 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = 2;
+        int expectedValue = 8;
+        int actualValue = calculatePow3(x);
+        assertEquals(expectedValue,actualValue);
+    }
+
+    public static int calculatePow3 (int x){
+        return (int) Math.pow(x,3);
+    }
+}
